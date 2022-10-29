b0ba4db802c5b6f635a0e22ab20a4139721dec69
Moritz Beyer <moritz.orlando.beyer@mac.com>
2022-10-29 12:34:44 +0200

    Initial Commit

---
+++ Apple Swift Package/Apple_Swift_PackageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Swift_PackageApp.swift
+//  Apple Swift Package
+//
+//  Created by Moritz Beyer on 29.10.22.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Swift_PackageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
